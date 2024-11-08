08655dceb9cd5107e0a447d8cb194b4ce544122a
Asad120600 <146355063+Asad120600@users.noreply.github.com>
2024-11-08 22:39:23 +0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,18 +3,12 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "invenflow",        // Your database name
-      useNewUrlParser: true,      // New URL parser (for backward compatibility)
-      useUnifiedTopology: true,  // Use the new Server Discover and Monitoring engine
-      useFindAndModify: false,   // Disable the legacy findAndModify() function
-      useCreateIndex: true,      // Ensure indexes are created automatically
+      dbName: "invenflow",
     });
-
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);  // Exit process if MongoDB connection fails
+    process.exit(1);
   }
 };
